613672fd5b346ef923b949b523c1b86f93f27218
RomanProkhorovich <pomaprokhorovich583@gmail.com>
2024-09-29 14:06:17 +0400

    Add jsoninclude

--- src/main/java/com/example/generatorRandomValues/dto/RequestField.java
+++ src/main/java/com/example/generatorRandomValues/dto/RequestField.java
@@ -1,5 +1,6 @@
 package com.example.generatorRandomValues.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.Setter;
 public class RequestField {
     private FieldType type;
     private String name;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String format;
 }
 
