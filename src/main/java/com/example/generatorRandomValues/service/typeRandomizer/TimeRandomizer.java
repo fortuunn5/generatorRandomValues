fd116e8e89c46287743aef830f592f802a180690
Viktoria <viktoriia.andreeva@gmail.com>
2024-10-02 19:03:02 +0400

    Refactor TimeRandomizer class
    Add MIN_SEC and MAX_SEC constants

--- src/main/java/com/example/generatorRandomValues/service/typeRandomizer/TimeRandomizer.java
+++ src/main/java/com/example/generatorRandomValues/service/typeRandomizer/TimeRandomizer.java
@@ -9,6 +9,9 @@ import java.util.Random;
 @Service
 public class TimeRandomizer implements TypeRandomizer {
 
+    private final int MIN_SEC = 0;
+    private final int MAX_SEC = 86399;
+
     @Override
     public FieldType getType() {
         return FieldType.TIME;
@@ -17,6 +20,6 @@ public class TimeRandomizer implements TypeRandomizer {
     @Override
     public LocalTime getRandomValue() {
         Random random = new Random();
-        return LocalTime.ofSecondOfDay(random.nextLong(86399));
+        return LocalTime.ofSecondOfDay(random.nextLong(MIN_SEC, MAX_SEC));
     }
 }
