77748c0e0a3753bcddecf0cddf6e1332b8450cc2
Viktoria <viktoriia.andreeva@gmail.com>
2024-10-03 20:21:11 +0400

    Add messages.properties
    Add messages.properties, add MessageConfig class

--- src/main/java/com/example/generatorRandomValues/controller/AdviceController.java
+++ src/main/java/com/example/generatorRandomValues/controller/AdviceController.java
@@ -1,5 +1,7 @@
 package com.example.generatorRandomValues.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,12 +9,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.temporal.UnsupportedTemporalTypeException;
+import java.util.Locale;
 
 @ControllerAdvice(annotations = RestController.class)
 public class AdviceController {
 
+    @Autowired
+    private MessageSource messageSource;
+
     @ExceptionHandler(UnsupportedTemporalTypeException.class)
-    public ResponseEntity<?> UnsupportedTemporalTypeException(UnsupportedTemporalTypeException e) {
-        return new ResponseEntity<>("Неподходящий шаблон форматирования", HttpStatus.BAD_REQUEST);
+    public ResponseEntity<?> unsupportedTemporalTypeException(UnsupportedTemporalTypeException e) {
+        String message = getMessage("unsupportedTemporalTypeException.message");
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+
+    private String getMessage(String messageKey) {
+        Locale locale = Locale.ROOT;
+        return messageSource.getMessage(messageKey, null, locale);
     }
 }
